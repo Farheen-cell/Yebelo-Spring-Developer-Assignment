541c2506c640abb325d89e6469ea1af2424faf00
farheen-omniful <farheen.ali@omniful.com>
2024-04-26 21:09:13 +0530

    creat project and add basic dependencies

---
+++ monopoly-game/monopoly-game/src/main/java/com/monopoly/MonopolyGameApplication.java
@@ -0,0 +1,13 @@
+package com.monopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyGameApplication.class, args);
+	}
+
+}
---
+++ monopoly-game/monopoly-game/src/test/java/com/monopoly/MonopolyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
